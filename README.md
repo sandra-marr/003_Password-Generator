804a00b75d2ed199e45f2aeadc105902ef6e3521
SPeats <srpeters44@gmail.com>
2021-04-10 15:05:08 -0700

    Added content the README.md file and updated the js to minimize the number of prompts and alerts that show up. General clean up of initial commit. Added comments into the js to help explain the code for future reference.

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # Homework03
-Homework03 - Password Generator
+## Password Generator
+
+The goal of this password generator is to first prompt the user for criteria that will then be used to generate a password. The criteria includes the desired length of the password, as well as the character types to include (uppercase letters, lowercase letters, numbers, and special characters).
+ext 
+Once the input is received, the user should see an alert that confirms all of the criteria going into the password, and finally, a password will be generated and ready to use. 
+
+## Prompts and Alerts
+
+This was an interesting project. I had to play around with the prompts and alerts alot to get the desired result. For example, if the user didn't select the correct length of password, they would have to be notified and the prompt would have to run again instead of moving to the next prompt. Another thing I considered when setting up the prompts was the amount of clicks that the user would have to go through before getting their password. Too many prompts and alerts could be very annoying. Rather than give feedback on choices after each prompt, I chose to summarize the user inputs in one window at the end of that cycle. 
+
+## Character Codes
+
+The next challenge was how to generate the values needed to populate the password. Rather than setting up arrays by typing out each letter, number, etc. I chose to use ranges from ASCII character codes for each character category. 
+
+Once that was done, I wanted to drop all of the posible character codes into one array to use when generating the password, however that would require insight into the criteria selected by the user. I used logical test to see if each character category should be in the final value array, and then built that. 
+
+## Generating the password
+
+Finally, a for loop was used to populate the password based in the length input given by the user. In this loop, I also used Math.floor and Math.random to pull random integers from the final value array and pushed them into the password value array. the last step was to convert the ASCII character codes to the actual characters, and display it on the screen. 
+
+
+Here is the link to the final product: https://sandra-marr.github.io/Homework03/
+
+![The Password Generator application displays a red button to "Generate Password".](./assets/images/03-javascript-homework-demo.png)
+
--- script.js
+++ script.js
@@ -13,23 +13,21 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-// goal- click generate password > prompt asks for input on 1. length between 8 and 128 characters. 2. character types (upper, lower, numeric, special), then a password is generated
+// goal- click generate password > prompt asks for input on 1. length between 8 and 128 characters. 2. character types (upper, lower, numeric, special), then a password is generated.
 
 function generatePassword(numberCharacters, upperCase, lowerCase, number, specialCharacters) {
 
-// define variables for each type of character
-
 //select the number of characters. This needs to repeat if the user provides the wrong input (i.e. they input  3 or 140). If the user hits cancel, this should not repeat. It should start the process over. 
   while (true) {
-    var numberCharacters = prompt("Please select the number of characters for your password. This should be between 8 and 128.", 8);
+    var numberCharacters = prompt("Please select the number of characters for your password. This should be between 8 and 128.", 8); // the defualt text will be 8 to prevent error if the user doesn't input a number.
     
     if (numberCharacters >= 8 && numberCharacters <= 128) {
       break; 
-    } 
+    } //when the input is in the range, jump out of the if loop and give feedback to the user on the input. Otherwise, continue the if loop.
     
     if(numberCharacters = "null"){
       alert("You have now stopped the generate password tool. To try again, click \"Generate Password\".")
-      return passwordText.value;
+      return "Your Secure Password"; //This will keep the page looking the same after the function is stopped.
     } 
     alert("That number is outside of the range. Please pick again.");
   }   
@@ -37,49 +35,50 @@ function generatePassword(numberCharacters, upperCase, lowerCase, number, specia
   
   console.log(numberCharacters);
   
-//Use uppercase? 
+  //Use uppercase? 
   var upperCase = confirm("If you would like to use uppercase letters in your password, please select okay.")
 
-  if (upperCase){
-    alert("Your password will contain uppercase letters.");
-  }
-
-  if (!upperCase){
-  alert("Your password will not contain uppercase letters.");
-  }
-
-// Lowercase?
+  // Use lowercase?
   var lowerCase = confirm("If you would like to use lowercase characters in your password, please select okay.")
 
-  if (lowerCase){
-    alert("Your password will contain lowercase letters.");
-  }
-
-  if (!lowerCase){
-  alert("Your password will not contain lowercase letters.");
-  }
-
-// Number?
+  // Use number?
   var number = confirm("If you would like to use numbers in your password, please select okay.")
-  
-  if (number){
-    alert("Your password will contain numbers.");
-  }
 
-  if (!number){
-  alert("Your password will not contain numbers.");
-  }
-
-// Special characters?
+  // Use special characters?
   var specialCharacters = confirm("If you would like to use special characters in your password, please select okay.")
 
-  if (specialCharacters){
-    alert("Your password will contain special characters.");
+  //ensure that at least one character type is selected
+  if(!upperCase && !lowerCase && !number && !specialCharacters) {
+    alert("Please select to use at least one character type.");
+   return "Your Secure Password";
   }
-  
-  if (!specialCharacters){
-  alert("Your password will not contain special characters.");
+
+  // alert the user of their choices
+  if (upperCase){
+    var useUpperCase = "uppercase, ";
+  } else {
+    var useUpperCase = "";
   }
+
+  if (lowerCase){
+   var useLowerCase = "lowercase, ";
+  } else {
+    var useLowerCase = "";
+  }
+
+  if (number){
+    var useNumber = "numbers, ";
+   } else {
+     var useNumber = "";
+   }
+
+   if (specialCharacters){
+    var useSpecialCharacters = "special characters.";
+   } else {
+     var useSpecialCharacters = "";
+   }
+   
+   alert("You have selected to use the following: " + useUpperCase + useLowerCase + useNumber + useSpecialCharacters);
 
 //develop baseline values to use per the inputs. The values are from ASCII character codes.
 
@@ -103,7 +102,7 @@ function generatePassword(numberCharacters, upperCase, lowerCase, number, specia
   var specialCharacterValues = generateValueArrays(33,47).concat(generateValueArrays(58,64)).concat(generateValueArrays(91,96)).concat(generateValueArrays(123,126));
   console.log(specialCharacterValues);
   
-//develop the full list of characters/values that will be used to select the password.
+//develop the full list of characters/values that will be used to select the password. Use the variables from the prompts earlier in the function. If Yes was selected, the var will be true and will add to the full list. If No wa selected, the var will be false, and not be included in the full list. 
   var allCharacters = [];
   if(upperCase){
     allCharacters = upperCaseValues;
@@ -121,12 +120,13 @@ function generatePassword(numberCharacters, upperCase, lowerCase, number, specia
 
  
 //create an array with the criteria above using a for loop. Each element in the array will need to be randomly generated and pushed into the array. The random numbers need to be integers. The max index value that can be returned is 93 since the allCharater.length is 94.
-
+  
   var passwordText = [];
   for (i = 0; i < numberCharacters; i++) {
     var value = allCharacters[Math.floor(Math.random() * allCharacters.length)];
     passwordText.push(String.fromCharCode(value));
   }
+
   console.log(passwordText);
   console.log(allCharacters.length);
 
